131897da9414271602b6e9d43dcade40f6f54df1
Ninjin <jimbienvenu@gmail.com>
2019-12-19 13:13:02 +0100

    WIP: Switching to linux lol windows sucks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate clap;
+use clap::{App, Arg};
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("MyPipe")
+        .version("1.0")
+        .about("Pipe operator implementation")
+        .author("Jim Bienvenu")
+        .arg(
+            Arg::with_name("in")
+                .short("i")
+                .long("in")
+                .value_name("input")
+                .help("Input command")
+                .takes_value(true)
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("out")
+                .short("o")
+                .long("out")
+                .value_name("ouput")
+                .help("Ouput command")
+                .takes_value(true)
+                .required(true)
+        )
+        .get_matches();
+
+        let input_command = matches.value_of("in").unwrap();
+        let output_command = matches.value_of("out").unwrap();
+
+
+        println!("Input : {} \nOutput : {}", input_command, output_command);
 }
